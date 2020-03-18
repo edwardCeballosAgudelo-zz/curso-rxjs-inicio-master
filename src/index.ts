2d63ce3e765fa863b8a893be73ad7cb68ce2a05e
eceballos <eceballos@transfiriendo.com>
2020-03-18 17:25:17 -0500

    observer y subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
+import { Observable, Observer } from 'rxjs'
+
+const observer: Observer <any> = {
+  next: value => console.log('siguientre [next]:', value),
+  error: error => console.warn('error [obs]:', error),
+  complete: () => console.info('completado [obs]')
+}
+
+// const obs$ = Observable.create();
+const obs$ = new Observable(subs => {
+  subs.next('Hola')
+  subs.next('Mundo')
 
 
-console.log('Hola Mundo!');
+  // forzar un error
+/*   const a = undefined;
+  a.nombre = 'edward'; */
 
+  subs.complete();
+});
 
+obs$.subscribe( observer )
 
-
-
-
-
+/* obs$.subscribe(
+  valor => console.log('next: ', valor),
+  error => console.warn('error: ', error),
+  () => console.info('completado')
+); */
