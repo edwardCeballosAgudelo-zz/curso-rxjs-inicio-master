a5c4471acbb3f7f3674be287aee029eddba74445
eceballos <eceballos@transfiriendo.com>
2020-04-07 06:23:28 -0500

    distinctUntilChanged

--- src/operadores/12-distinctUntilChanged.ts
+++ src/operadores/12-distinctUntilChanged.ts
@@ -1,5 +1,11 @@
 import { of, from } from "rxjs";
-import { distinctUntilChanged } from "rxjs/operators";
+import { distinct, distinctUntilChanged } from "rxjs/operators";
+
+const numeros$ = of<number|string>(1, '1', 1, 1, 3, 3 , 2, 2, 4, 4, 5, 3, 1, '1');
+
+numeros$.pipe(
+  distinct() // ===
+).subscribe( console.log );
 
 interface Personaje {
   nombre: string
