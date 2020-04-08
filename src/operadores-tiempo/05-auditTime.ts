f2b89b38f38a1db2937070a20790c46a577c4086
eceballos <eceballos@transfiriendo.com>
2020-04-08 06:21:03 -0500

    auditTime

---
+++ src/operadores-tiempo/05-auditTime.ts
@@ -0,0 +1,10 @@
+import { fromEvent } from "rxjs";
+import { tap, auditTime, map } from "rxjs/operators";
+
+const click$ = fromEvent<MouseEvent>( document, 'click' );
+
+click$.pipe(
+  map(({ x }) => x ),
+  tap(val => console.log('tap', val)),
+  auditTime(2000)
+).subscribe( console.log );
